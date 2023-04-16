9f88148103986d3e5c93d1ff9f86b2150fca91a5
Lucas Casal <lucasocteixeira@gmail.com>
2023-04-16 00:43:09 -0300

    recebe msg e usuários

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 axios.defaults.headers.common['Authorization'] = 'mnAwsXuL5brrrSMP21IIYkpu';
 const feed = document.querySelector("#feed");
+const userOptions = document.querySelector(".contacts-options");
 var username = 0;
+
+// entrar no chat e enviar o nome;
 function enterChat(){
     username = document.querySelector("#username").value;
     const usernameSent={
         name: username
     };
-
-
     const promise = axios.post(
         "https://mock-api.driven.com.br/api/vm/uol/participants",
         usernameSent
@@ -17,17 +18,16 @@ function enterChat(){
         const promiseOnline = axios.post(
             "https://mock-api.driven.com.br/api/vm/uol/status",
             usernameSent
-        )
-        promise.then(console.log(promise));
+        );
     };
     setInterval(stayOnline, 5000);
 
     const idScreen = document.querySelector(".id-screen");
     idScreen.classList.add("hidden");
-    console.log(username);
 }
+    //
 
-
+    //envio de mensagem;
 function idGenerator(size) {
     var randomString = '';
     var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
@@ -98,6 +98,71 @@ function message(){
 }
 
 
+function searchMessages(){
+    const promise = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
+    promise.then(processarResposta); 
+
+    function processarResposta(resposta) {
+        console.log(resposta.data);
+        for (i=0; i<resposta.data.length; i++){
+            const receivedMessage = resposta.data[i];
+            console.log(receivedMessage);
+
+            printMessages(receivedMessage);
+            
+        }
+    }
+
+    function printMessages(recebido){
+        const messageBox = document.createElement("div");
+            
+        messageBox.className = "text-box";
+        feed.appendChild(messageBox);
+
+        const headerId = idGenerator(26)
+        const header = document.createElement("span");
+        header.className = "header";
+        header.setAttribute('id', headerId);
+        messageBox.appendChild(header);
+
+        const messageAt = document.createElement("span");
+        messageAt.className = "time";
+        messageAt.innerText = "(" + recebido.time + ")";
+        header.appendChild(messageAt);
+        
+        const messageFrom = document.createElement("span");
+        messageFrom.className = "names";
+        messageFrom.innerText = recebido.from;
+        header.appendChild(messageFrom);
+
+        const textingType = document.createElement("span");
+        textingType.className = "texting-type";
+        textingType.innerText = recebido.type;
+        header.appendChild(textingType);
+
+        const messageTo = document.createElement("span");
+        messageTo.className = "names";
+        messageTo.innerText = recebido.to + ":";
+        header.appendChild(messageTo);
+
+        const thisId = idGenerator(100);
+        const messageText = document.createElement("span");
+        messageText.setAttribute("id", thisId);
+        messageText.innerText = recebido.text;
+
+        messageBox.appendChild(header);
+        messageBox.appendChild(messageText);
+
+        const headerWidth = document.getElementById(headerId).offsetWidth;
+
+        const indent = headerWidth + 5 + "px";
+        console.log(thisId)
+        document.getElementById(thisId).style.textIndent = indent;
+    }
+    
+}
+setInterval(searchMessages, 3000);
+
 const temporaryBackground = document.querySelector(".ready-to-chat");
 const contactsList = document.querySelector(".contacts-list");
 
@@ -106,42 +171,178 @@ function openContactsList(){
     contactsList.classList.add("show");
     contactsList.classList.remove("hide");
     temporaryBackground.classList.remove("hidden");
-    console.log(contactsList);
 }
 
 function backToChat(){
     contactsList.classList.remove("show");
     temporaryBackground.classList.add("hidden");
-    contactsList.classList.add("hide");
-    console.log(contactsList)
+    contactsList.classList.add("hide");    
 }
 
-const onlineUser = document.createElement("div");
-onlineUser.className = "online-user-box";
-contactsList.appendChild(onlineUser);
 
-const onlineUserName = document.createElement("span");
-onlineUserName.className = "online-user-name";
 
-const onlineUserIcon = document.createElement("span");
-onlineUserIcon.className = "online-user-icon"
-onlineUserIcon.setAttribute("img", "images/");
+// ver e registrar os usuários online
+function updateUsers(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/vm/uol/participants");
+    promise.then(onlineUsers);
+    function onlineUsers(resposta) {
+        const usernames = [];
+        for (i=0; i<resposta.data.length; i++){
 
-function selectedOption(option){
+            const usernameFound = resposta.data[i].name;
+            usernames.push(usernameFound);
+        }   
+    
+        for (i=0; i<usernames.length; i++){
+            const verify = usernames[i];
+            var verifiedWith=0
+
+            for (x=0; x<userOptions.children.length; x++){
+                if (verify === userOptions.children[x].innerText){
+                    verifiedWith = -100;
+                } else{
+                    verifiedWith++;
+                }
+            }
+            if (verifiedWith === userOptions.children.length){
+                const onlineUser = document.createElement("div");
+                onlineUser.className = "user-box";
+                userOptions.appendChild(onlineUser);
+                    
+                const onlineUserIcon = document.createElement("img");
+                onlineUserIcon.className = "contacts-list-icons";
+                onlineUserIcon.setAttribute("src", "images/person-circle.svg");
+            
+                const onlineUserName = document.createElement("span");
+                onlineUserName.className = "online-user-name";
+                onlineUserName.innerText = verify;
+            
+                onlineUser.appendChild(onlineUserIcon);
+                onlineUser.appendChild(onlineUserName);
+                onlineUser.setAttribute("onclick", "selectedOptionUser(this)");
+                userOptions.appendChild(onlineUser);
+            
+            } else{}
+        } 
+        
+        for (x=1; x<userOptions.children.length; x++){
+            const testUser = userOptions.children[x]
+            const testUserT = testUser.innerText;
+            var verified = 0;   
+            for (i=0; i<usernames.length; i++){
+                if(testUserT === usernames[i]){
+                    verified = -100;
+                } else {
+                    verified++;
+                }
+            }
+            if (verified === usernames.length){
+                userOptions.removeChild(testUser);
+            }
+
+        }
+        
+
+    }
+
+    
+}
+
+setInterval(updateUsers, 3000);
+
+//
+
+
+
+
+
+//colocar o check
+
+function selectedOptionUser(userOption){
+    const onlineUsersOptions = document.querySelectorAll(".user-box");
     checkIcon = document.createElement("img");
     checkIcon.setAttribute("src", "images/checkmark.svg")
     checkIcon.classList.add("check");
+    checkIcon.setAttribute("id", "user-check-marked")
 
-    console.log(option.children[0]);
-        if (option.childElementCount === 2){
-            console.log(option.children[1]);
-            option.removeChild(option.children[1]);
-        } else{
-            option.appendChild(checkIcon);
+    for (i=0; i< onlineUsersOptions.length; i++){
+        if (onlineUsersOptions[i] !== userOption && onlineUsersOptions[i].querySelector("#user-check-marked")){
+         onlineUsersOptions[i].removeChild(document.getElementById("user-check-marked")) 
         }
 
-
-    console.log(checkIcon);
-    console.log(option);
+    }
+    if (userOption.childElementCount === 3){    
+    } else{
+    userOption.appendChild(checkIcon);
+    }
 }
 
+function selectedOptionType(typeOption){
+    const onlineTypesOptions = document.querySelectorAll(".type-box");
+    checkIcon = document.createElement("img");
+    checkIcon.setAttribute("src", "images/checkmark.svg")
+    checkIcon.classList.add("check");
+    checkIcon.setAttribute("id", "type-check-marked")
+
+    for (i=0; i< onlineTypesOptions.length; i++){
+        if (onlineTypesOptions[i] !== typeOption && onlineTypesOptions[i].querySelector("#type-check-marked")){
+         onlineTypesOptions[i].removeChild(document.getElementById("type-check-marked")) 
+        }
+
+    }
+    if (typeOption.childElementCount === 3){    
+    } else{
+    typeOption.appendChild(checkIcon);
+    }
+
+        
+}
+
+// 
+
+
+/*
+            const receivedMessage = resposta.data[i];
+            
+            const messageBox = document.createElement("div");
+            
+            messageBox.className = "text-box";
+            feed.appendChild(messageBox);
+
+            const header = document.createElement("span");
+            header.setAttribute('id', 'header');
+            messageBox.appendChild(header);
+
+            const messageAt = receivedMessage.time;
+            header.appendChild(messageAt);
+
+            const messageFrom = document.createElement("span");
+            messageFrom.className = "names";
+            messageFrom.innerText = receivedMessage.from;
+            header.appendChild(messageFrom);
+
+            const textingType = document.createElement("span");
+            textingType.className = "texting-type";
+            textingType.innerHTML = "para";
+            header.appendChild(textingType);
+
+            const messageTo = document.createElement("span");
+            messageTo.className = "names";
+            messageTo.innerText = receivedMessage.to + ":";
+            header.appendChild(messageTo);
+
+            const thisId = idGenerator(26);
+            const messageText = document.createElement("span");
+            messageText.setAttribute("id", thisId);
+            messageText.innerText = receivedMessage.text;
+
+            messageBox.appendChild(header);
+            messageBox.appendChild(messageText);
+
+            var headerWidth = document.getElementById("header").offsetWidth;
+
+            var indent = headerWidth + 5 + "px";
+
+            document.getElementById(thisId).style.textIndent = indent;
+            */
+            
